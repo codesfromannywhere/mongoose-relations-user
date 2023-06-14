705d3d0bd3882a327234fada6dd02869d4314388
codesfromannywhere <codesfromannywhere@gmail.com>
2023-06-14 15:09:39 +0200

    added posts to users

--- index.js
+++ index.js
@@ -25,31 +25,39 @@ const postModel = model("Post", postSchema)
 // ====== CREATE NEW DATA ======
 
 
-// const user = await userModel.create({
-//     name: "Moon Goose",
-//     age: 47,
-// })
-// const user2 = await userModel.create({
-//     name: "Rosa Blume",
-//     age: 27
-// })
-// const user3 = await userModel.create({
-//     name: "Moon Goose",
-//     age: 41
-// })
-
-
-
-
-const post = new postModel({
-    title: "When live gives you lemon",
-    content: "... you can make some fresh lemon juice.",
+const user = await userModel.create({
+    name: "Lemon Ade",
+    age: 22,
 })
 
-post.author.push(post);
 
-await post.save()
+const post1 = new postModel(
+    {
+        title: "When live gives you lemon",
+        content: "... you can make some fresh lemon juice.",
+    }
+);
 
+
+const post2 = new postModel(
+    {
+        title: "Easy Peasy",
+        content: "lemon squeezyyyyy."
+    }
+);
+
+// ====== HIER WIRD EINE USER-ID EINEM POST ZU "gepusht/zugeordnet" ======
+
+post1.author.push(user);
+await post1.save()
+
+post2.author.push(user);
+await post2.save()
+
+
+
+// const postforUser = await userModel.findById("6489b3a5a63ef6e46581ae5f")
+// console.log(postforUser);
 
 
 await mongoose.disconnect();
